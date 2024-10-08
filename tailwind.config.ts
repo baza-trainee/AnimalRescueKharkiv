b3457e98433284f45efd4daf0ebb2c31ff4d00a2
baza-trainee <132754508+baza-trainee@users.noreply.github.com>
2024-10-08 18:25:47 +0200

    tailwind cass

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      md: '768px',
+      lg: '960px',
+      xl: '1280px',
+      xlr: '1440px',
+      '2xl': '1920px',
+    },
     extend: {
       colors: {
         background: "var(--background)",
