daba39a2b9c859a816ba1028d580603ed3b21c58
baza-trainee <132754508+baza-trainee@users.noreply.github.com>
2024-10-09 19:08:51 +0200

    CRM Header

---
+++ src/components/crm/Header/index.tsx
@@ -0,0 +1,13 @@
+const HeaderCrm = () => {
+    return (
+        <header className="border-b border-gray-300">
+            <div className="h-[36px] px-6 ">
+                <div className="flex">
+                    <p>Статистика</p>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default HeaderCrm;
