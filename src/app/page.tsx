130ae253db4cba6ea6cc2a4a0464c295a3a359e8
Maryna <marimariovod@gmail.com>
2024-10-09 14:28:54 +0300

    create main page
    didn't add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Header from "../components/common/Header";
-import Footer from '../components/common/Footer';
+import Link from "next/link";
+import Main from "../components/common/MainPage";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <div>
-        <h1 className="underline" >Main Page</h1>
-        <p>This is the Main page.</p>
+      <div className="flex flex-col justify-items-center">
+      <h1 className="mx-auto text-center mt-20 mb-36 font-bold text-2xl text-black" >CRM СИСТЕМА<br/>
+Animal Rescue Kharkiv</h1>
+      <div className="mx-auto mb-40"><Main/></div>
+      <Link href="/login"  className="mx-6  py-3 rounded-lg bg-[#4855CC] text-white text-center font-normal text-[20px] leading-[30px]">Увійти</Link>
       </div>
-      <Footer />
-    </>
+    
   )
 }
