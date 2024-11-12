922d7cdf4805a69d0ca210945b7d03c04f43d828
baza-trainee <132754508+baza-trainee@users.noreply.github.com>
2024-11-12 19:41:48 +0100

    TotalCounter component created

--- src/app/crm/page.tsx
+++ src/app/crm/page.tsx
@@ -1,11 +1,14 @@
 import FooterCrm from "@/src/components/crm/Footer";
 import HeaderCrm from "@/src/components/crm/Header";
+import TotalCounterCrm from "@/src/components/crm/TotalCounterCrm";
 
 export default function CRMPage() {
   return (
     <>
       <HeaderCrm />
-      <div>Content</div>
+      <section className="mx-6">
+        <TotalCounterCrm />
+      </section>
       <FooterCrm/>
     </>
   );
---
+++ src/components/crm/TotalCounterCrm/index.tsx
@@ -0,0 +1,9 @@
+const TotalCounterCrm = () => {
+    return (
+        <div className="flex my-6 border border-mainBlue rounded-[10px] p-4">
+            <p>Всього тварин в АRK</p>
+        </div>
+    )
+}
+
+export default TotalCounterCrm;
