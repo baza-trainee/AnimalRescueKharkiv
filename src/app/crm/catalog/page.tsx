90d39da1283e9648604dbab110ac549490ce0048
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-01-23 11:06:21 +0200

    add header crm

--- src/app/crm/catalog/page.tsx
+++ src/app/crm/catalog/page.tsx
@@ -1,12 +1,12 @@
 import CatalogCrm from "@/src/components/crm/CatalogCrm";
 import FooterCrm from "@/src/components/crm/Footer";
-import HeaderCrm from "@/src/components/crm/Header";
+import Header from "@/src/components/crm/Header";
 
 
 export default function CatalogPage() {
   return (
     <>
-      <HeaderCrm />
+      <Header />
       <section className="mx-6">
         <CatalogCrm />
       </section>
