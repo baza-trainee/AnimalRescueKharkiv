95d19d22e250b79349eb1393c44b3fb7387de47a
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-25 14:59:13 +0200

    Change menu style

--- src/app/crm/catalog/page.tsx
+++ src/app/crm/catalog/page.tsx
@@ -1,11 +1,13 @@
 import CatalogCrm from "@/src/components/crm/CatalogCrm";
 import FooterCrm from "@/src/components/crm/Footer";
+import MenuDesktop from "@/src/components/crm/MenuDesktop";
 
 export default function CatalogPage() {
   return (
     <>
       {/* <HeaderCrm /> */}
-      <section className="mx-6">
+      <section className="flex">
+        <MenuDesktop />
         <CatalogCrm />
       </section>
       <FooterCrm />
