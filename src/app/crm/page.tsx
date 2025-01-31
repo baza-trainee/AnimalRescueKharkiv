c5b6ef7d7af2f113792e43ea31cf0bdc481bf5a7
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-01-31 13:59:01 +0200

    disabling the desktop version of CRM

--- src/app/crm/page.tsx
+++ src/app/crm/page.tsx
@@ -3,16 +3,16 @@
 import CountryStatistic from "@/src/components/crm/CountryStatistic";
 import DepartmentStatistic from "@/src/components/crm/DepartmentStatistic";
 import FooterCrm from "@/src/components/crm/Footer";
-import MenuDesktop from "@/src/components/crm/MenuDesktop";
+// import MenuDesktop from "@/src/components/crm/MenuDesktop";
 // import HeaderCrm from "@/src/components/crm/Header";
 
 export default function CRMPage() {
   return (
     <>
       <main className="w-full flex-col">
-        <section className="w-full flex">
+        <section className="w-full">
           {/* <HeaderCrm /> */}
-          <MenuDesktop />
+          {/* <MenuDesktop /> */}
           <DepartmentStatistic />
           <CountryStatistic />
         </section>
