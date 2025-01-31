0b2585189fc61c2113f21ab1ae8e431e152083e9
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-01-31 15:55:10 +0200

    add package "react-datepicker"

--- src/app/crm/page.tsx
+++ src/app/crm/page.tsx
@@ -1,29 +1,23 @@
-'use client'
+"use client";
 
-import Header from "@/src/components/crm/Header/index";
 import CountryStatistic from "@/src/components/crm/CountryStatistic";
 import DepartmentStatistic from "@/src/components/crm/DepartmentStatistic";
 import FooterCrm from "@/src/components/crm/Footer";
+import Header from "@/src/components/crm/Header";
 // import MenuDesktop from "@/src/components/crm/MenuDesktop";
 // import HeaderCrm from "@/src/components/crm/Header";
 
 export default function CRMPage() {
   return (
     <>
+      {" "}
+      <Header title="Статистика" />
       <main className="w-full flex-col">
-        <section className="w-full">
+        <section className="">
           {/* <HeaderCrm /> */}
           {/* <MenuDesktop /> */}
           <DepartmentStatistic />
           <CountryStatistic />
-          <MenuDesktop />
-          <div className="flex-col">
-            <Header title="Cтатистика" />
-            <div className="flex">
-              <DepartmentStatistic />
-              <CountryStatistic />
-            </div>
-          </div>
         </section>
         <FooterCrm />
       </main>
