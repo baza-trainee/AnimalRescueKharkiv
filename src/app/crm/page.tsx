10bd81b0afac046d39f4b02a4c87edef3a89e180
Maryna <marimariovod@gmail.com>
2025-02-03 15:17:34 +0200

    add date picker without back

--- src/app/crm/page.tsx
+++ src/app/crm/page.tsx
@@ -5,6 +5,7 @@ import DepartmentStatistic from "@/src/components/crm/DepartmentStatistic";
 import FooterCrm from "@/src/components/crm/Footer";
 import Header from "@/src/components/crm/Header";
 import LatesrAdd from "@/src/components/crm/LatestAdd";
+import DateRangePicker from "@/src/components/crm/Statistic";
 // import MenuDesktop from "@/src/components/crm/MenuDesktop";
 // import HeaderCrm from "@/src/components/crm/Header";
 
@@ -18,6 +19,7 @@ export default function CRMPage() {
           {/* <HeaderCrm /> */}
           {/* <MenuDesktop /> */}
           <DepartmentStatistic />
+          <DateRangePicker/>
           <CountryStatistic />
           <LatesrAdd/>
         </section>
