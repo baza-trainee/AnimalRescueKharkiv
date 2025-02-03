75e13fa71c3e844cff789291ed135e3432202b88
Maryna <marimariovod@gmail.com>
2025-02-03 19:15:50 +0200

    fix deploying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
    <div>
-      <h1><a href="./crm">CRM Page</a></h1>
+      {/* <h1><a href="./crm">CRM Page</a></h1> */}
       
       <p>This is the CRM page.</p>
     </div>
