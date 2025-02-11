f3e5ebda4dda4d214f058894722bdef670f87cc4
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-02-11 13:12:23 +0200

    add exit tu crm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
+import Mainlogo from './mainlogo/page'
 export default function Home() {
   return (
    <div>
-      <h1><a href="./crm">CRM Page</a></h1>
-      
-      <p>This is the CRM page.</p>
+      {/* <h1><a href="./crm">CRM Page</a></h1> */}
+      <Mainlogo/>
+      {/* <p>This is the CRM page.</p> */}
     </div>
     
   )
