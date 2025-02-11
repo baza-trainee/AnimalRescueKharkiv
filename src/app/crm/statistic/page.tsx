11db11bca09c651b1633de66f98c7eefa66a0d57
Maryna <marimariovod@gmail.com>
2025-02-11 13:49:54 +0200

    fix redirect on pages

---
+++ src/app/crm/statistic/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import CountryStatistic from "@/src/components/crm/CountryStatistic";
+import DepartmentStatistic from "@/src/components/crm/DepartmentStatistic";
+import FooterCrm from "@/src/components/crm/Footer";
+import Header from "@/src/components/crm/Header";
+import LatesrAdd from "@/src/components/crm/LatestAdd";
+import DateRangePicker from "@/src/components/crm/Statistic";
+// import MenuDesktop from "@/src/components/crm/MenuDesktop";
+// import HeaderCrm from "@/src/components/crm/Header";
+
+export default function CRMPage() {
+  return (
+    <>
+      {" "}
+      <Header title="Статистика" />
+      <main className="w-full flex-col">
+        <section className="">
+          {/* <HeaderCrm /> */}
+          {/* <MenuDesktop /> */}
+          <DepartmentStatistic />
+          <DateRangePicker/>
+          <CountryStatistic />
+          <LatesrAdd/>
+        </section>
+        <FooterCrm />
+      </main>
+    </>
+  );
+}
