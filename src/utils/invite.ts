9847a7e459dce1e952faf452e64c2f63bf2c487e
Maryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2025-03-27 13:54:08 +0200

    fix structure refresh password api

--- src/utils/invite.ts
+++ src/utils/invite.ts
@@ -4,11 +4,11 @@ import { post } from "./api";
 export const sendInvitation = async (email: string, role: string, language: string, domain: string) => {
     return post(`/auth/invite/${domain}`, { email, role, language })
         .then((data) => {
-            console.log("Invitation sent:", data);
+            console.log("Запрошення відправлено:", data);
             return data;
         })
         .catch((error) => {
-            console.error("Failed to send invitation:", error);
+            console.error("Помилка відправки запрошення:", error);
             throw error;
         });
 }
