b9b5eeea85147b7c9a7bcc4372255bc4e2ed19b5
VeronikaVaraksina1 <plaguemoon@gmail.com>
2025-03-31 14:28:28 +0300

    conflict resolution

--- src/components/crm/AddCardCrm/schemas/addCardSchema.ts
+++ src/components/crm/AddCardCrm/schemas/addCardSchema.ts
@@ -8,168 +8,168 @@ today.setHours(0, 0, 0, 0);
 const formattedTodayDate = format(new Date(), "dd.MM.yyyy");
 
 export const addCardSchema = Yup.object().shape({
-  // name: Yup.string()
-  //   .matches(nameRegExp, "Введіть коректне ім'я")
-  //   .min(2, "Ім’я має бути не менше 2 символів")
-  //   .max(30, "Ім’я має бути не більше 30 символів")
-  //   .required("Введіть ім’я"),
-  // origin__arrival_date: Yup.mixed()
-  //   .transform((value) =>
-  //     value instanceof Date ? format(value, "dd/MM/yyyy") : value
-  //   )
-  //   .nullable()
-  //   .required("Дата прибуття обов’язкова")
-  //   .test(
-  //     "no-later-than-today",
-  //     `Не пізніше за ${formattedTodayDate}`,
-  //     function (value) {
-  //       if (!value || typeof value !== "string") return true;
-  //       const parsedDate = parse(value, "dd/MM/yyyy", new Date());
-  //       return isValid(parsedDate) && parsedDate <= today;
-  //     }
-  //   ),
-  // origin__city: Yup.string()
-  //   .min(2, "Місто має бути не менше 2 символів")
-  //   .max(100, "Місто має бути не більше 100 символів")
-  //   .required("Введіть місто"),
-  // origin__address: Yup.string().nullable().notRequired(),
-  // general__animal_type: Yup.object().shape({
-  //   id: Yup.number().nullable().required("Оберіть тип тварини"),
-  // }),
-  // general__gender: Yup.string().required("Оберіть стать тварини"),
-  // general__weight: Yup.number().notRequired(),
-  // general__age: Yup.number().notRequired(),
-  // general__specials: Yup.string().nullable().notRequired(),
-  // owner__info: Yup.string().nullable().notRequired(),
-  // comment__text: Yup.string().nullable().notRequired(),
-  // locations: Yup.array()
-  //   .of(
-  //     Yup.object().shape({
-  //       location: Yup.object().shape({
-  //         id: Yup.number().nullable().required("Оберіть локацію"),
-  //       }),
-  //       date_from: Yup.mixed()
-  //         .transform((value) =>
-  //           value instanceof Date ? format(value, "dd/MM/yyyy") : value
-  //         )
-  //         .nullable()
-  //         .test(
-  //           "no-later-than-today",
-  //           `Не пізніше за ${formattedTodayDate}`,
-  //           function (value) {
-  //             if (!value || typeof value !== "string") return true;
-  //             const parsedDate = parse(value, "dd/MM/yyyy", new Date());
-  //             return isValid(parsedDate) && parsedDate <= today;
-  //           }
-  //         )
-  //         .test("required-date-from", "Оберіть дату 'З'", function (value) {
-  //           const { location } = this.parent;
-  //           if (location?.id && !value) {
-  //             return this.createError({ message: "Оберіть дату 'З'" });
-  //           }
-  //           return true;
-  //         }),
-  //       date_to: Yup.mixed()
-  //         .transform((value) =>
-  //           value instanceof Date ? format(value, "dd/MM/yyyy") : value
-  //         )
-  //         .nullable()
-  //         .test(
-  //           "no-later-than-today",
-  //           `Не пізніше за ${formattedTodayDate}`,
-  //           function (value) {
-  //             if (!value || typeof value !== "string") return true;
-  //             const parsedDate = parse(value, "dd/MM/yyyy", new Date());
-  //             return isValid(parsedDate) && parsedDate <= today;
-  //           }
-  //         )
-  //         .test("date-to-after-from", 'Не раніше дати "З"', function (value) {
-  //           const { date_from } = this.parent;
-  //           if (!date_from || !value) {
-  //             return true;
-  //           }
-  //           return value >= date_from;
-  //         }),
-  //     })
-  //   )
-  //   .test(
-  //     "current-location-is-required",
-  //     "Оберіть поточну локацію",
-  //     function (value) {
-  //       if (!value || !value[0]?.location?.id) {
-  //         return this.createError({
-  //           path: "locations.0.location.id",
-  //           message: "Оберіть поточну локацію",
-  //         });
-  //       }
-  //       return true;
-  //     }
-  //   ),
-  // sterilization__done: Yup.boolean().notRequired(),
-  // sterilization__date: Yup.mixed()
-  //   .nullable()
-  //   .transform((value) =>
-  //     value instanceof Date ? format(value, "dd/MM/yyyy") : value
-  //   )
-  //   .test(
-  //     "no-later-than-today",
-  //     `Не пізніше за ${formattedTodayDate}`,
-  //     function (value) {
-  //       if (!value || typeof value !== "string") return true;
-  //       const parsedDate = parse(value, "dd/MM/yyyy", new Date());
-  //       return isValid(parsedDate) && parsedDate <= today;
-  //     }
-  //   )
-  //   .notRequired(),
-  // sterilization__comment: Yup.string()
-  //   .max(500, "Коментар не може бути більше 500 символів")
-  //   .notRequired(),
-  // microchipping__done: Yup.boolean().notRequired(),
-  // microchipping__date: Yup.mixed()
-  //   .nullable()
-  //   .transform((value) =>
-  //     value instanceof Date ? format(value, "dd/MM/yyyy") : value
-  //   )
-  //   .test(
-  //     "no-later-than-today",
-  //     `Не пізніше за ${formattedTodayDate}`,
-  //     function (value) {
-  //       if (!value || typeof value !== "string") return true;
-  //       const parsedDate = parse(value, "dd/MM/yyyy", new Date());
-  //       return isValid(parsedDate) && parsedDate <= today;
-  //     }
-  //   )
-  //   .notRequired(),
-  // microchipping__comment: Yup.string()
-  //   .max(500, "Коментар не може бути більше 500 символів")
-  //   .notRequired(),
-  // vaccinations: Yup.array()
-  //   .of(
-  //     Yup.object({
-  //       is_vaccinated: Yup.boolean().notRequired(),
-  //       vaccine_type: Yup.string().notRequired(),
-  //       date: Yup.mixed()
-  //         .nullable()
-  //         .transform((value) =>
-  //           value instanceof Date ? format(value, "dd/MM/yyyy") : value
-  //         )
-  //         .test(
-  //           "no-later-than-today",
-  //           `Не пізніше за ${formattedTodayDate}`,
-  //           function (value) {
-  //             if (!value || typeof value !== "string") return true;
-  //             const parsedDate = parse(value, "dd/MM/yyyy", new Date());
-  //             return isValid(parsedDate) && parsedDate <= today;
-  //           }
-  //         )
-  //         .notRequired(),
-  //       comment: Yup.string()
-  //         .max(500, "Коментар не може бути більше 500 символів")
-  //         .notRequired(),
-  //     })
-  //   )
-  //   .notRequired()
-  //   .default([]),
+  name: Yup.string()
+    .matches(nameRegExp, "Введіть коректне ім'я")
+    .min(2, "Ім’я має бути не менше 2 символів")
+    .max(30, "Ім’я має бути не більше 30 символів")
+    .required("Введіть ім’я"),
+  origin__arrival_date: Yup.mixed()
+    .transform((value) =>
+      value instanceof Date ? format(value, "dd/MM/yyyy") : value
+    )
+    .nullable()
+    .required("Дата прибуття обов’язкова")
+    .test(
+      "no-later-than-today",
+      `Не пізніше за ${formattedTodayDate}`,
+      function (value) {
+        if (!value || typeof value !== "string") return true;
+        const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+        return isValid(parsedDate) && parsedDate <= today;
+      }
+    ),
+  origin__city: Yup.string()
+    .min(2, "Місто має бути не менше 2 символів")
+    .max(100, "Місто має бути не більше 100 символів")
+    .required("Введіть місто"),
+  origin__address: Yup.string().nullable().notRequired(),
+  general__animal_type: Yup.object().shape({
+    id: Yup.number().nullable().required("Оберіть тип тварини"),
+  }),
+  general__gender: Yup.string().required("Оберіть стать тварини"),
+  general__weight: Yup.number().notRequired(),
+  general__age: Yup.number().notRequired(),
+  general__specials: Yup.string().nullable().notRequired(),
+  owner__info: Yup.string().nullable().notRequired(),
+  comment__text: Yup.string().nullable().notRequired(),
+  locations: Yup.array()
+    .of(
+      Yup.object().shape({
+        location: Yup.object().shape({
+          id: Yup.number().nullable().required("Оберіть локацію"),
+        }),
+        date_from: Yup.mixed()
+          .transform((value) =>
+            value instanceof Date ? format(value, "dd/MM/yyyy") : value
+          )
+          .nullable()
+          .test(
+            "no-later-than-today",
+            `Не пізніше за ${formattedTodayDate}`,
+            function (value) {
+              if (!value || typeof value !== "string") return true;
+              const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+              return isValid(parsedDate) && parsedDate <= today;
+            }
+          )
+          .test("required-date-from", "Оберіть дату 'З'", function (value) {
+            const { location } = this.parent;
+            if (location?.id && !value) {
+              return this.createError({ message: "Оберіть дату 'З'" });
+            }
+            return true;
+          }),
+        date_to: Yup.mixed()
+          .transform((value) =>
+            value instanceof Date ? format(value, "dd/MM/yyyy") : value
+          )
+          .nullable()
+          .test(
+            "no-later-than-today",
+            `Не пізніше за ${formattedTodayDate}`,
+            function (value) {
+              if (!value || typeof value !== "string") return true;
+              const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+              return isValid(parsedDate) && parsedDate <= today;
+            }
+          )
+          .test("date-to-after-from", 'Не раніше дати "З"', function (value) {
+            const { date_from } = this.parent;
+            if (!date_from || !value) {
+              return true;
+            }
+            return value >= date_from;
+          }),
+      })
+    )
+    .test(
+      "current-location-is-required",
+      "Оберіть поточну локацію",
+      function (value) {
+        if (!value || !value[0]?.location?.id) {
+          return this.createError({
+            path: "locations.0.location.id",
+            message: "Оберіть поточну локацію",
+          });
+        }
+        return true;
+      }
+    ),
+  sterilization__done: Yup.boolean().notRequired(),
+  sterilization__date: Yup.mixed()
+    .nullable()
+    .transform((value) =>
+      value instanceof Date ? format(value, "dd/MM/yyyy") : value
+    )
+    .test(
+      "no-later-than-today",
+      `Не пізніше за ${formattedTodayDate}`,
+      function (value) {
+        if (!value || typeof value !== "string") return true;
+        const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+        return isValid(parsedDate) && parsedDate <= today;
+      }
+    )
+    .notRequired(),
+  sterilization__comment: Yup.string()
+    .max(500, "Коментар не може бути більше 500 символів")
+    .notRequired(),
+  microchipping__done: Yup.boolean().notRequired(),
+  microchipping__date: Yup.mixed()
+    .nullable()
+    .transform((value) =>
+      value instanceof Date ? format(value, "dd/MM/yyyy") : value
+    )
+    .test(
+      "no-later-than-today",
+      `Не пізніше за ${formattedTodayDate}`,
+      function (value) {
+        if (!value || typeof value !== "string") return true;
+        const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+        return isValid(parsedDate) && parsedDate <= today;
+      }
+    )
+    .notRequired(),
+  microchipping__comment: Yup.string()
+    .max(500, "Коментар не може бути більше 500 символів")
+    .notRequired(),
+  vaccinations: Yup.array()
+    .of(
+      Yup.object({
+        is_vaccinated: Yup.boolean().notRequired(),
+        vaccine_type: Yup.string().notRequired(),
+        date: Yup.mixed()
+          .nullable()
+          .transform((value) =>
+            value instanceof Date ? format(value, "dd/MM/yyyy") : value
+          )
+          .test(
+            "no-later-than-today",
+            `Не пізніше за ${formattedTodayDate}`,
+            function (value) {
+              if (!value || typeof value !== "string") return true;
+              const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+              return isValid(parsedDate) && parsedDate <= today;
+            }
+          )
+          .notRequired(),
+        comment: Yup.string()
+          .max(500, "Коментар не може бути більше 500 символів")
+          .notRequired(),
+      })
+    )
+    .notRequired()
+    .default([]),
   diagnoses: Yup.array()
     .of(
       Yup.object({
