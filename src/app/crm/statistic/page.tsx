a364a95b69a75b0b1083b987f08e75495ff4d3c7
Maryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2025-04-15 13:24:41 +0300

    add date range for country statistic,fix bags with calendar

--- src/app/crm/statistic/page.tsx
+++ src/app/crm/statistic/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 import CountryStatistic from "@/src/components/crm/CountryStatistic";
 import DepartmentStatistic from "@/src/components/crm/DepartmentStatistic";
@@ -6,21 +7,29 @@ import FooterCrm from "@/src/components/crm/Footer";
 import Header from "@/src/components/crm/Header";
 import LastAnimals from "@/src/components/crm/LatestAdd";
 import DateRangePicker from "@/src/components/crm/Statistic";
-// import MenuDesktop from "@/src/components/crm/MenuDesktop";
-// import HeaderCrm from "@/src/components/crm/Header";
+
+
+const today = new Date();
+const oneMonthAgo = new Date();
+oneMonthAgo.setMonth(today.getMonth() - 1);
 
 export default function CRMPage() {
+   const [startDate, setStartDate] = useState<Date | null>(oneMonthAgo);
+  const [endDate, setEndDate] = useState<Date | null>(today);
   return (
     <>
       {" "}
       <Header title="Статистика" />
       <main className="w-full flex flex-col justify-items-center">
         <section className="w-[390px] mx-auto">
-          {/* <HeaderCrm /> */}
-          {/* <MenuDesktop /> */}
           <DepartmentStatistic />
-          <DateRangePicker />
-          <CountryStatistic />
+          <DateRangePicker
+          startDate={startDate}
+            endDate={endDate}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}/>
+          <CountryStatistic
+          startDate={startDate} endDate={endDate}/>
           <LastAnimals />
         </section>
       </main>
