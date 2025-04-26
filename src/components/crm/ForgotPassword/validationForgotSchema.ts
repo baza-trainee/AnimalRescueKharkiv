e0be8b7b3f28a8fa729ceaa1d5b456b150cfe5c9
Maryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2025-04-26 20:59:15 +0300

    fixed validation in auth

--- src/components/crm/ForgotPassword/validationForgotSchema.ts
+++ src/components/crm/ForgotPassword/validationForgotSchema.ts
@@ -1,15 +1,16 @@
 import * as Yup from "yup";
 
 const emailRegexp =
-  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?!ru$|by$|рф$)[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
 
 export const forgotSchema = Yup.object().shape({
-   email: Yup.string()
-      .matches(emailRegexp, "Введіть дійсний email")
-      .required("Введіть email"),
+  email: Yup.string()
+    .email( "Введіть дійсний email")
+   .matches(emailRegexp, "Введіть дійсний email(не допускаються домени .ru, .by, .рф)")
+    .required("Введіть email"),
  
 });
 
 
-export type TypeForgotSchema = Yup.InferType<typeof forgotSchema>;
+export type TypeForgotSchema = Yup.InferType<typeof forgotSchema>;  
--- src/components/crm/Login/validationLoginSchema.ts
+++ src/components/crm/Login/validationLoginSchema.ts
@@ -1,8 +1,11 @@
   import * as Yup from "yup";
 
+  const emailRegexp =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?!ru$|by$|рф$)[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
   export const loginSchema = Yup.object().shape({
     email: Yup.string()
+      .matches(emailRegexp, "Введіть дійсний email(не допускаються домени .ru, .by, .рф)")
       .email( "Введіть дійсний email")
       .required("Введіть email"),
     password: Yup.string()
